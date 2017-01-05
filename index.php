250abfda752e6a2dd3ab679c5d96f770912ec6e1
Dimitris Katsoulis <Dimitris Katsoulis>
2017-01-06 01:58:11 +0200

    eshop Html, Css

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <title>Our First HTML5 Page</title>
+  <meta name="description" content="Welcome to my basic template.">
+  <link rel="stylesheet" href="css/style.css?v=1">
+</head>
+ 
+<body>
+    <div id="wrapper">
+      <header>
+      	<div class="logo"><img class="logo_image" src="images/logo.png"></div>
+	    <nav>
+		    <ul>
+			    <li><a rel="external" href="#">HOME</a></li>
+			    <li><a rel="external" href="#">ABOUT US</a></li>
+			    <li><a rel="external" href="#">CONTACT</a></li>
+		    </ul>
+	    </nav>
+        <div class="offers">
+           	<div class="offer">
+           		<div tooltip="info info info info info info info infoinfo info info infoinfo info info infoinfo info info infoinfo info info info">
+        			<img class="offer_image" src="images/shoe.jpg">
+        		</div>
+        		<div class="price">100€</div>
+        	</div>
+        	<div class="offer">
+        		<img class="offer_image" src="images/shoe.jpg">
+        		<div class="price">100€</div>
+        	</div>
+        	<div class="offer">
+        		<img class="offer_image" src="images/shoe.jpg">
+        		<div class="price">100€</div>
+        	</div>
+        	<div class="offer">
+        		<img class="offer_image" src="images/shoe.jpg">
+        		<div class="price">100€</div>
+        	</div>
+        	<div class="offer">
+        		<img class="offer_image" src="images/shoe.jpg">
+        		<div class="price">100€</div>
+        	</div>
+        	<div class="offer">
+        		<img class="offer_image" src="images/shoe.jpg">
+        		<div class="price">100€</div>
+        	</div>
+        	<div class="offer">
+        		<img class="offer_image" src="images/shoe.jpg">
+        		<div class="price">100€</div>
+        	</div>
+        	<div class="offer">
+        		<img class="offer_image" src="images/shoe.jpg">
+        		<div class="price">100€</div>
+        	</div>
+        </div>    
+        </header>
+        <footer>
+        	<div class="footer"></div>    	
+        </footer>
+    </div>
+</body>
+</html>
